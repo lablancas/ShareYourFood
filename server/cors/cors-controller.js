bf75f9f2c3ac31dea562be904db1958392ad52c4
lablancas <lablancas@gmail.com>
2014-05-09 18:06:24 -0500

    simplified the CORS controller

--- server/cors/cors-controller.js
+++ server/cors/cors-controller.js
@@ -5,12 +5,7 @@
  */
 
 exports.cors = function(req, res, next) {
-        if (!req.get('Origin')) return next();
-        // use "*" here to accept any origin
-        res.set('Access-Control-Allow-Origin', '*'); // http://localhost:3000
-        res.set('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-        res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
-        // res.set('Access-Control-Allow-Max-Age', 3600);
-        if ('OPTIONS' === req.method) return res.send(200);
-        next();
-    };
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9090');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	next();
+};
